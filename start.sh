15e0984934e1b9727b89f05387e98da91087705c
khainee <118953358+khainee@users.noreply.github.com>
2022-12-19 13:43:07 +0630

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+pkg update
+pkg upgrade 
+pkg install git
+pkg install python
+pip install --upgrade pip
+pip install pyrogram
+git clone https://github.com/khainee/session-string && cd session-string
+python generate_string_session.py
