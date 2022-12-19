f1b18d86616d538d26a6bbc62efc67c0f5a9130c
khainee <118953358+khainee@users.noreply.github.com>
2022-12-19 14:16:10 +0630

    Create start2.sh

---
+++ start2.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade 
+apt install git
+apt install python
+pip3 install --upgrade pip
+pip3 install pyrogram
+git clone https://github.com/khainee/session-string && cd session-string
+python3 generate_string_session.py
